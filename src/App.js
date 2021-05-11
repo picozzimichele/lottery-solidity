68c011c0b8775d04d5b0bc8387b634db3a20987b
picozzimichele <michele.picozzi@hotmail.it>
2021-05-11 19:24:32 +0100

    backbone finished and working on Test Network

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from "./web3";
+import lottery from "./lottery";
+import React, { useState, useEffect } from 'react';
+import { useForm } from "react-hook-form";
 
 function App() {
+  const [manager, setManager] = useState("loading manager address...");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("0");
+  const [message, setMessage] = useState("")
+  const { handleSubmit } = useForm()
+
+  console.log(value)
+
+  
+  useEffect(() => {
+    async function fetchManager() { //do not need to specify which account this is from as metamask already chooses the account
+      const manager = await lottery.methods.manager().call(); 
+      setManager(manager)
+    } 
+
+    fetchManager();
+
+    async function getPlayers() {
+      const players = await lottery.methods.getPlayers().call();
+      setPlayers(players);
+    }
+
+    getPlayers();
+
+    async function getBalance() {
+      const balance = await web3.eth.getBalance(lottery.options.address);
+      setBalance(balance);
+    }
+
+    getBalance();
+
+  }, []);
+
+  const onSubmit = async () => {  
+    const accounts = await web3.eth.getAccounts();
+
+    const message = "waiting on transaction success..."
+    setMessage(message);
+
+    await lottery.methods.enter().send({
+      from: accounts[0], 
+      value: web3.utils.toWei(value, "ether")
+    });
+
+    const messageTwo = "Transaction has succeded!"
+    setMessage(messageTwo);
+  };
+
+  const pickWinner = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    const message = "waiting on transaction success..."
+    setMessage(message);
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    })
+
+    const messageWin = "a winner has been picked!"
+    setMessage(messageWin);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>this contract is managed by {manager}</p>
+      <p>the number of players competing to win the lottery contract is {players.length}</p>
+      <p>the balance of the contract is {web3.utils.fromWei(balance, "ether")} ether</p>
+      <hr />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>amount of ether to enter</label>
+          <input 
+            value = {value}
+            onChange={event => setValue(event.target.value)}
+          />
+          <button>Enter</button>
+        </div>
+      </form>
+      <hr />
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={handleSubmit(pickWinner)}>Pick a winner</button>
+      <hr />
+      <h1>{message}</h1>
     </div>
   );
 }
---
+++ src/lottery.js
@@ -0,0 +1,76 @@
+import web3 from "./web3";
+
+const address = "0x262aeDAF84A09EA2B38c548144E843f5467048bF";
+
+
+//this is the abi that comes from the lottery contract, is essentialy a copy of the existing contract deployed in the address above
+
+const abi = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getPlayers",
+		"outputs": [
+			{
+				"internalType": "address payable[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+
+export default new web3.eth.Contract(abi,address); //this is our window to the contract that expists on the blockchain test network
